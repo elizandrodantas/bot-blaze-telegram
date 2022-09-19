c908d170beb9a88399003d76481ad7328331301d
elizandrodantas <dantaspm@hotmail.com>
2022-09-18 21:18:02 -0300

    update examples

--- example/example-with-dotenv.mjs
+++ example/example-with-dotenv.mjs
@@ -16,17 +16,15 @@ figlet('Blaze with Telegram', (_, screen) => {
 async function start(){
     let appOra = ora('Iniciando aplicação').start(),
         controllerBot = new BotBlazeWithTelegram({
-            sticker: {
-                winNotGale: "win.jpg",
-                winGaleOne: "win-in-gale.jpg",
-                winGaleTwo: "win-in-gale.jpg",
-                winWhite: "win-white.jpg",
-                loss: "loss.jpg",
-            },
-            enterProtection: true,
             timeAfterWin: {
                 message: "Tempo para analise apos win",
                 time: 1
+            },
+            sticker: {
+                win: "win.jpg",
+                winGale: "win-in-gale.jpg",
+                winWhite: "win-white.jpg",
+                loss: "loss.jpg",
             },
             timeAfterLoss: {
                 time: 1,
@@ -34,12 +32,23 @@ async function start(){
             },
             summaryOfResult: {
                 interval: 1,
-                message: (number, info, cb) => {
-                    cb('mensagem sobressalente');
-                    
-                    return `Total de jogadas: ${number.total}\nWins seguidos: ${number.consecutive} ✅\nTotal de win: ${number.win} ✅\nTotal de loss: ${number.loss} ❌\nTaxa de acertividade: ${(number.win / number.total * 100).toFixed(1)}%`
+                message: (number) => {
+                    return `Total de jogadas: ${number.total}` +
+                        `\nWins seguidos: ${number.consecutive} ✅` +
+                        `\nTotal de win: ${number.win} ✅` +
+                        `\nTotal de loss: ${number.loss} ❌` +
+                        `\nTaxa de acertividade: ${(number.win / number.total * 100).toFixed(1)}%`
                 }
-            }
+            },
+            messageEnterBet: (current, recents, cb) => {
+                // cb('test callback');
+        
+                return "🔎 <b>SINAL ENCONTRADO:</b>\n" +
+                    `\nENTRE NO ${_getColorNameOrEmoticon(current.color, { emoticon: true })} ${_getColorNameOrEmoticon(current.color, { pt: true, upper: true })}` +
+                    `\nPROTEJA NO ${_getColorNameOrEmoticon(0, { emoticon: true })} ${_getColorNameOrEmoticon(0, { pt: true, upper: true })}` +
+                    `\n\n<pre>https://blaze.com/${process.env.REF ? "r/" + process.env.REF : ""}</pre>`;
+            },
+            gale: 1
          })
 
     await controllerBot.run();
